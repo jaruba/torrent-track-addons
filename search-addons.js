ddc91e78699db93256ea97040a98295fa51fa493
Alexandru Branza <branza.alex@gmail.com>
2020-09-09 23:55:34 +0300

    Update search-addons.js

--- search-addons.js
+++ search-addons.js
@@ -10,8 +10,9 @@ module.exports = [
   },
   {
     name: 'RARBG',
-    url: 'https://rarbgp2p.org/torrents.php?category=%%type%%&search=%%query%%&order=seeders&by=DESC',
+    url: 'https://rarbgp2p.org/torrents.php?category=%%type%%&search=%%imdb%%&order=seeders&by=DESC',
     space: '+',
+    useIds: ['imdb'],
     types: {
       movie: '14;48;17;44;45;47;50;51;52;42;46;54',
       series: '18;41;49',
@@ -19,17 +20,29 @@ module.exports = [
   },
   {
     name: 'ThePirateBay',
-    url: 'https://thepiratebay.org/search.php?q=%%query%%&video=on&search=Pirate+Search&page=0&orderby=',
+    url: 'https://thepiratebay.org/search.php?q=%%imdb%%&video=on&search=Pirate+Search&page=0&orderby=',
+    useIds: ['imdb'],
     space: '+'       
   },
   {
-    name: 'Google',
-    url: 'http://www.google.com/search?q=%%query%%',
-    space: '+'
+    name: 'Open Directories',
+    url: 'https://www.google.com/search?q=intext%3A%5C%22%%query%%%5C%22+intitle%3A%5C%22index.of%5C%22+%2B%28wmv%7Cmpg%7Cavi%7Cmp4%7Cmkv%7Cmov%29+-inurl%3A%28jsp%7Cpl%7Cphp%7Chtml%7Caspx%7Chtm%7Ccf%7Cshtml%29',
+    noQuotes: true,
+    space: '+'       
   },
   {
     name: 'JustWatch',
     url: 'https://www.themoviedb.org%%tmdbUrl%%/watch',
     useIds: ['tmdbUrl']
+  },
+  {
+    name: 'Wikipedia',
+    url: 'https://en.wikipedia.org/w/index.php?search=%%query%%&title=Special%3ASearch&go=Go&ns0=1',
+    space: '+'
+  },
+  {
+    name: 'Google',
+    url: 'http://www.google.com/search?q=%%query%%',
+    space: '+'
   }
 ]
